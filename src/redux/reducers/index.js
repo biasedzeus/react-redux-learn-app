59f31d3b52c10e2a6783af5bcae53e5a1b87e74a
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-26 20:27:03 +0530

    updated name

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,7 @@
 // This file is used to combine all reducer .
 
 import { combineReducers } from "redux";
-import { productReducer } from "./product.reducer";
+import { productReducer } from "./productReducer";
 
  const reducers = combineReducers({
     allProducts:productReducer
