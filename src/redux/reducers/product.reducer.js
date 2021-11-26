6bd332f41e9c0a7e66d3c4cd20e9ad7b931d9353
Bhanu Singh <bhanu.singh1078@gmail.com>
2021-11-26 20:06:25 +0530

    state and productReducer added

---
+++ src/redux/reducers/product.reducer.js
@@ -0,0 +1,24 @@
+import { actionTypes } from "../constants/actionTypes"
+
+const initialState = {
+    products=[{
+        id:1,
+        title:"Naruto",
+        category:"Ninja"
+    }]
+}
+
+
+export const productReducer = (state,{type,payload}) =>
+{
+    switch (type) {
+        case actionTypes.SET_PRODUCTS:
+            return state;
+            
+            
+    
+        default:
+            return state;
+           
+    }
+}
